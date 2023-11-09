c5d421795ecb65d853f79772dc7ea1b51c772f5f
Abhijit Parad <94881938+ErzaXAbhi@users.noreply.github.com>
2023-11-09 17:28:52 +0530

    updated

---
+++ script.js
@@ -0,0 +1,5 @@
+// const heroBtn = document.getElementById("heroBtn");
+// const heroImg = document.getElementById("heroImg");
+
+// gsap.to(heroBtn, { y: -10, ease: "linear", duration: 1 });
+// gsap.from(heroImg, { y: 500, delay: 0 });
